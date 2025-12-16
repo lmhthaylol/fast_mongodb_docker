04de042bf6e48143b0fcf8925b30296fc3c1ee7b
lmhthaylol <143104079+lmhthaylol@users.noreply.github.com>
2025-12-16 11:55:10 +0700

    reload database

--- config/connection.py
+++ config/connection.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 MONGO_URI = "mongodb+srv://sonchuvan1104_db_user:Sonphong123@cluster0.hpj7o5k.mongodb.net/?appName=Cluster0"
-DATABASE_NAME = "book-model-db"
+DATABASE_NAME = "college"
 
 client = AsyncIOMotorClient(
     MONGO_URI,
